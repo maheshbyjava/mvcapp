c195410f29da3c323f69f3baba0cbe62a9b9d2aa
Home <Home@Welcome>
2018-09-05 15:28:22 -0400

    2nd file

--- src/com/app/model/Employee.java
+++ src/com/app/model/Employee.java
@@ -2,15 +2,11 @@ package com.app.model;
 
 public class Employee {
 private int empId;
-private String empname;
-public Employee(int empId, String empname) {
-	super();
-	this.empId = empId;
-	this.empname = empname;
-}
+ private String empName;
+
 @Override
 public String toString() {
-	return "Employee [empId=" + empId + ", empname=" + empname + "]";
+	return "Employee [empId=" + empId + ", empName=" + empName + "]";
 }
 
 }
